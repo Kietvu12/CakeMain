d4e05d6abf50fbed34a570b98a83308949bc3edc
Kietvu12 <118310455+Kietvu12@users.noreply.github.com>
2023-05-12 22:51:56 +0700

    Add files via upload

---
+++ Cake_SQL.sql
@@ -0,0 +1,45 @@
+create database QuanLyTiemBanh
+
+create table Customer(
+tenKH nvarchar(100) not null,
+soDT char(10) primary key,
+diemKH int,
+);
+go
+create table LoaiHH(
+MaLoai int primary key identity(1,1),
+TenLoai nvarchar(205) not null,
+SoLuong int not null,
+);
+go
+create table Hh(
+MaHH int primary key identity(1,1),
+MaLoai int not null,
+DonGia int not null,
+SoLuong int not null,
+);
+go
+drop table Hh;
+create table Hh(
+MaHH int primary key identity(1,1),
+MaLoai int not null,
+DonGia int not null,
+Soluong int not null,
+);
+go
+create table HoaDon(
+MaHD int primary key identity(1,1),
+soDT char(10) not null,
+NgayDat Datetime default getdate(),
+TongTien int not null,
+);
+go
+create table ChiTietHoaDon(
+MaHD int not null,
+MaHH int not null,
+soLuong int not null,
+donGia int not null,
+thanhTien int not null,
+); 
+go
+
